205284dc4afcd36219d08b99f7086d54f63092a9
Juan Arias <juan.arias@marist.edu>
2017-02-05 12:19:08 -0500

    Feedback on lab 2

--- labs/lab 2/Problem4.java
+++ labs/lab 2/Problem4.java
@@ -13,17 +13,18 @@ public class Problem4 {
 		//Vowels
 		 if(ch=='a' || ch=='A' || ch=='e' || ch=='E' ||
 			        ch=='i' || ch=='I' || ch=='o' || ch=='O' ||
-			        ch=='u' || ch=='U');
+			        ch=='u' || ch=='U'); // JA: This is a syntax error. You are closing the if statement with the ;
 		 {
 			 System.out.println("This is a vowel.");
 		 }
 		 //Invalid characters
+		 // JA: The else statement will be invalid because the if was already closed
 		 else if(ch=='!' || ch=='@' || ch=='#' || ch=='$' ||
 				 ch=='%' || ch=='^' || ch=='&' || ch=='*' ||
 				 ch=='(' || ch==')' || ch=='-' || ch=='=' ||
 				 ch=='+' || ch=='_' || ch=='?' || ch=='.' ||
 				 ch==',' || ch=='>' || ch=='<' || ch=='~' ||
-				 ch=='`');
+				 ch=='`'); // JA: This is a syntax error. You are closing the if statement with the ;
 		 {
 			 System.out.println("This is an invalid character.");
 		 }
